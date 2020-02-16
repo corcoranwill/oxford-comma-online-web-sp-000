06631f8d5f3468643ef37b34fa3ad250cc1e7ee7
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 02:04:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,44 @@
 def oxford_comma(array)
   string = ""
+  case array.length
+  when array.length == 0
+    string << array[0]
+  when array.length == 1
+    string << array[0]
+    string << "and"
+    string << array[1]
+  end
+  string
+end
+
+
+
+
+
+def oxford_comma(array)
+  string = ""
   counter = 0
   array.each do |element|
+    case array.length
+    when array.length == 0
+      "string1"
+    when array.length == 1
+      "string1 and string2"
+    else
+      "string1, string2, string3, string4, and string5"
+    end
+
+
+
+
+
+
+
+
     # adds first element in array to an empty string
     string << element
     puts string
+
   end
   puts "FINAL STRING: #{string}"
   string
