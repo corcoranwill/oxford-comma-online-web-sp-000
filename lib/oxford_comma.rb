c5551a864db184c770394efa3e31f39550aec1d4
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 01:54:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
   array.each do |element|
     # adds first element in array to an empty string
     string << element
+    puts string
   end
+  puts "FINAL STRING: #{string}"
   string
 end
