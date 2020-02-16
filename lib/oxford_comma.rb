ffae0ef816bf3f71e30c0778c29ec3ff9666f8e3
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 02:16:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
+# utilize array.count (returns the number of elements in array)
+# =>could also use array.length or array.size
+# utilize 
+
 def oxford_comma(array)
   if array.count == 1
     array.join
   elsif array.count == 2
     array.join(" and ")
-  elsif array.count >= 3
+  else array.count >= 3
     last_one = array.pop
     list = array.join(", ")
     list << ", and "
