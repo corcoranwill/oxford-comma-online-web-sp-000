970e874432b2e902d27136f26461358d65dc2d2a
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 01:50:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,3 +10,20 @@ def oxford_comma(array)
   end
   string
 end
+
+def oxford_comma(array)
+  string = ""
+  counter = 0
+  array.each do |element|
+    # adds first element in array to an empty string
+    string << element
+    if counter == array.length
+      string << "and"
+    end
+    if counter >= 1
+      string << ","
+    end
+    counter += 1
+  end
+  string
+end
