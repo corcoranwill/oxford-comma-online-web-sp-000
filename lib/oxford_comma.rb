b82b81ea714b6b55799e306229cf077f6ea3df05
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 01:53:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,16 +4,6 @@ def oxford_comma(array)
   array.each do |element|
     # adds first element in array to an empty string
     string << element
-    puts string
-    if counter == array[-1]
-      string << "and"
-      puts string
-    end
-    if counter > 0
-      string << ", "
-      puts string
-    end
-    counter += 1
   end
   string
 end
