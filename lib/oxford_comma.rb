fa05a85baf2489981021abee6f3a71c86c7f6f47
Will Corcoran <corcoranwill@gmail.com>
2020-02-16 01:42:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,12 @@
 def oxford_comma(array)
   string = ""
+  counter = 0
   array.each do |element|
     string << element
+    if counter >= 1
+      string << ","
+    end
+    counter += 1
   end
   string
 end
